2b524cfafddc0fe6066c405f119823ffae881e9d
Benjamin Sejas <benjamin.sejas@gmail.com>
2017-10-05 00:20:31 +1100

    Fix adding links

--- api/controllers/topicListController.js
+++ api/controllers/topicListController.js
@@ -50,27 +50,81 @@ exports.delete_a_topic = function (req, res) {
 exports.create_a_link_for_topic = function (req, res) {
     console.log('params topic Id:', req.params.topicId);
     console.log('body topic Id:', req.body.topicId);
-    // Update existing link if it exists
-    Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.params.topicId)},{"links.topicId":ObjectId(req.body.topicId)}]},
-        {$set:{"links.$.description":req.body.description, "links.$.name":req.body.name}}, function (err, topic) {
-            console.log('err:', err);
-            console.log('topic:', topic);
-            if (err)
-                res.send(err);
 
-            if (!topic) {
-                // Create new link when it doesn't exist
-                Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.params.topicId)}]},
-                    {$addToSet:{"links":{$each:[{"topicId": ObjectId(req.body.topicId),"description":req.body.description,"links.$.name":req.body.name}]}}}, function(err, addedTopic) {
-                        console.log('err2:', err);
-                        console.log('topic2:', addedTopic);
-                        if (err) {
-                            res.send(err);
-                        }
-                        res.json(addedTopic);
-                    })
-            } else {
-                res.json(topic);
-            }
-        });
+    Topic.findById(req.params.topicId, function (err, topic) {
+        if (err)
+            res.send(err);
+
+        const topicName = topic.name;
+
+        // Update existing link if it exists
+        Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.params.topicId)},{"links.topicId":ObjectId(req.body.topicId)}]},
+            {$set:{"links.$.description":req.body.description, "links.$.name":req.body.name}}, function (err, topic) {
+                console.log('err:', err);
+                console.log('topic:', topic);
+                if (err)
+                    res.send(err);
+
+                if (!topic) {
+                    // Create new link when it doesn't exist
+                    Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.params.topicId)}]},
+                        {$addToSet:{"links":{$each:[{"topicId": ObjectId(req.body.topicId),"description":req.body.description,"name":req.body.name}]}}}, function(err, addedTopic) {
+                            console.log('err2:', err);
+                            console.log('topic2:', addedTopic);
+                            if (err) {
+                                res.send(err);
+                            }
+
+                            // Update existing link for other Link
+                            Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.body.topicId)},{"links.topicId":ObjectId(req.params.topicId)}]},
+                                {$set:{"links.$.description":req.body.description, "links.$.name":topicName}}, function (err, topic) {
+                                    console.log('err:', err);
+                                    console.log('topic:', topic);
+                                    if (err)
+                                        res.send(err);
+
+                                    if (!topic) {
+                                        // Create new link when it doesn't exist
+                                        Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.body.topicId)}]},
+                                            {$addToSet:{"links":{$each:[{"topicId": ObjectId(req.params.topicId),"description":req.body.description,"name":topicName}]}}}, function(err, addedTopic) {
+                                                console.log('err2:', err);
+                                                console.log('topic2:', addedTopic);
+                                                if (err) {
+                                                    res.send(err);
+                                                }
+                                                res.json(addedTopic);
+                                            })
+                                    } else {
+                                        res.json(topic);
+                                    }
+                                });
+                        })
+                } else {
+                    // Update existing link for other Link
+                    Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.body.topicId)},{"links.topicId":ObjectId(req.params.topicId)}]},
+                        {$set:{"links.$.description":req.body.description, "links.$.name":topicName}}, function (err, topic) {
+                            console.log('err:', err);
+                            console.log('topic:', topic);
+                            if (err)
+                                res.send(err);
+
+                            if (!topic) {
+                                // Create new link when it doesn't exist
+                                Topic.findOneAndUpdate({$and:[{"_id":ObjectId(req.body.topicId)}]},
+                                    {$addToSet:{"links":{$each:[{"topicId": ObjectId(req.params.topicId),"description":req.body.description,"name":topicName}]}}}, function(err, addedTopic) {
+                                        console.log('err2:', err);
+                                        console.log('topic2:', addedTopic);
+                                        if (err) {
+                                            res.send(err);
+                                        }
+                                        res.json(addedTopic);
+                                    })
+                            } else {
+                                res.json(topic);
+                            }
+                        });
+                }
+            });
+    });
+
 };
